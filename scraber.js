87d723d93265ff9acdd5d3310d946abbe690ff27
Mads Jon Nielsen <madsjon@gmail.com>
2019-10-31 21:19:31 +0100

    Made the world pretty. Seriously speed up scraber.js

--- scraber.js
+++ scraber.js
@@ -2,8 +2,8 @@ const puppeteer = require('puppeteer');
 const sleep = require('await-sleep');
 const fs = require('fs-extra');
 
-const reportId = "pQtgP2q3NDRb7XFz";
-const date = new Date("Wed Oct 23 2019 19:30 GMT+2");
+const reportId = "yXWjh39CdfpRgkxM";
+const date = new Date("Wed Oct 30 2019 19:30 GMT+2");
 const fileName = date.toISOString().replace(/:/g, "").replace(/\./g, "");
 
 console.log(fileName);
@@ -13,7 +13,7 @@ console.log(fileName);
 
     const page = await browser.newPage();
     const url = `https://classic.warcraftlogs.com/reports/${reportId}#boss=-2&difficulty=0&type=summary`;
-    await page.goto(url, {waitUntil: 'load'});
+    await page.goto(url, {waitUntil: 'networkidle2', timeout: 0});
 
     const sources = await page.evaluate(() => {
         const sources = new Map();
@@ -32,19 +32,17 @@ console.log(fileName);
         }
         return Array.from(sources.values());
     });
-    await browser.close();
+
+    await page.close();
 
     for (const source of sources) {
         const sourceId = source.sourceId;
         const clazz = source.clazz;
         const charName = source.charName;
 
-        const browser = await puppeteer.launch();
         const page = await browser.newPage();
         const url = `https://classic.warcraftlogs.com/reports/${reportId}#boss=-2&difficulty=0&source=${sourceId}&type=summary`;
-        await page.goto(url, {waitUntil: 'load'});
-
-        await sleep(2000);
+        await page.goto(url, {waitUntil: 'networkidle2', timeout: 0});
 
         try {
             const items = await page.evaluate(() => {
@@ -83,7 +81,9 @@ console.log(fileName);
             console.log(url);
             console.log(e);
         } finally {
-            await browser.close();
+            await page.close();
         }
     }
+
+    await browser.close();
 })();
